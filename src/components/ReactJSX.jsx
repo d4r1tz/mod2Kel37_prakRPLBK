c40cba65c33a8c6ffc72dd56d0afce71f4a68390
Daniel Ritz <d4r1tz@gmail.com>
2023-09-21 20:41:29 +0700

    Update ReactJSX.jsx

--- src/components/ReactJSX.jsx
+++ src/components/ReactJSX.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const asisten = {
-    name : 'Anas Noor Hakim',
+const praktikan = {
+    name : 'Daniel Ritz',
     jurusan : 'Teknik Komputer 2020'
 };
 
-function greetings(asisten){
-    return asisten.name + ' dari ' + asisten.jurusan;
+function greetings(praktikan){
+    return praktikan.name + ' dari ' + praktikan.jurusan;
 }
 
 //menggunakan JSX
@@ -14,7 +14,7 @@ const ReactJSX = () => {
     return (
         <div>
             <h1>Belajar JSX</h1>
-            <h2>Belajar JSX --- {greetings(asisten)}</h2>
+            <h2>Belajar JSX --- {greetings(praktikan)}</h2>
             <p style={{color:'#00ff00'}}>ini adalah contoh menggunakan JSX</p>
         </div>
     );
@@ -25,10 +25,10 @@ const ReactNoJSX = () => {
     return React.createElement(
         'div', null, 
         React.createElement('h1', null, 'Halo Praktikan hehe'),
-        React.createElement('h2', null, 'Nama saya ', greetings(asisten)), 
+        React.createElement('h2', null, 'Nama saya ', greetings(praktikan)), 
         React.createElement('p', {style:{color:'#ff0000'}}, 'ini adalah contoh tanpa JSX')
     )
 }
- 
+
 export default ReactJSX;
 export {ReactNoJSX};
